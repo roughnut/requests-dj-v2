7f549709a9640e62db868481acd693c18f09f4ec
Lachlan <lachyhughes@bigpond.com>
2024-08-05 20:15:57 +0930

    Added basic routing for the baseline pages, home, login, signup and event. Event will have later pages per ID and the main page will just be a placeholder when no event is found with the ID.

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+// Include code here for a Navbar.
+export default function Navbar() {
+    return (
+      <div>
+        <a href="/login">Login</a>
+        <a href="/signup">Signup</a>
+        <a href="/event">Event</a>
+        <a href="/">Home</a>
+      </div>
+    );
+  }
