a489cf247284a3f52c376d2d8d40e34994c8ef80
Lachlan <lachyhughes@bigpond.com>
2024-08-08 18:17:07 +0930

    Removed test disco ball from v1

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import "../App.css";
 export default function Navbar() {
     return (
       <div>
+        <a className="nav" href="/">Home</a>
         <a className="nav" href="/login">Login</a>
         <a className="nav" href="/signup">Signup</a>
         <a className="nav" href="/event">Event</a>
-        <a className="nav" href="/">Home</a>
       </div>
     );
   }
