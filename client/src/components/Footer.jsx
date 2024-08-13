e994cb0731cce5aa0d5dda625fe371d339e824ac
roughnut <jeremy@writever.com.au>
2024-08-13 18:53:46 +1000

    refactor footer styles from tailwind to bootstrap

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import '../App.css';
 
-// CSS style still in Tailwind, needs to be converted to Bootstrap.
-
 const Footer = () => {
   return (
-    <footer className="w-full bottom-0 left-0 right-0 mx-auto bg-custom-charcoal py-4 flex-wrap text-center mt-8">
-      <div className="text-white mt-4">
-        <div className="flex justify-center gap-4">
-          <a href="https://github.com/CinosMagician" target="_blank" className="hover:text-custom-mandalay hover:no-underline navi">Lachlan</a>
-          <a href="https://github.com/roughnut" target="_blank" className="hover:text-custom-mandalay hover:no-underline navi">Jeremy</a>
+    <footer className="w-100 fixed-bottom bg-light py-1 text-center mt-3">
+      <div className="text-dark mt-4">
+        <div className="d-flex justify-content-center gap-4">
+          <a href="https://github.com/CinosMagician" target="_blank" className="text-decoration-none navi">Lachlan</a>
+          <a href="https://github.com/roughnut" target="_blank" className="text-decoration-none navi">Jeremy</a>
         </div>
-        <p className="text-custom-brown mx-auto text-sm mt-2">&copy; 2024</p>
+        <p className="text-muted small mt-2">&copy; 2024</p>
       </div>
     </footer>
   );
