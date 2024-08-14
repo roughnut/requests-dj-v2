ce5a55fada151dd245fd7f1d6e1a11a9ba55960b
roughnut <jeremy@writever.com.au>
2024-08-15 09:08:52 +1000

    updated event page with current event info

--- client/src/pages/EventPage.jsx
+++ client/src/pages/EventPage.jsx
@@ -2,33 +2,51 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { useParams, useNavigate } from 'react-router-dom';
 import SongsList from '../components/SongsList';
-import { GET_SONG_REQUESTS } from '../utils/queries';
+import { GET_EVENT, GET_SONG_REQUESTS } from "../utils/queries";
 
 const EventPage = () => {
   const { id } = useParams(); // Retrieve the event ID from the URL
   const navigate = useNavigate();
-  const { loading, error, data } = useQuery(GET_SONG_REQUESTS, {
-    variables: { event: id},
+
+  // Query to get event details
+  const {
+    loading: eventLoading,
+    error: eventError,
+    data: eventData,
+  } = useQuery(GET_EVENT, {
+    variables: { _id: id },
   });
 
-  if (loading) return <p>Loading events...</p>;
-  if (error) return <p>Error loadng events: {error.message}</p>;
+  // Query to get song requests
+  const {
+    loading: songsLoading,
+    error: songsError,
+    data: songsData,
+  } = useQuery(GET_SONG_REQUESTS, {
+    variables: { event: id },
+  });
+if (eventLoading || songsLoading) return <p>Loading...</p>;
+if (eventError) return <p>Error loading event: {eventError.message}</p>;
+if (songsError) return <p>Error loading song requests: {songsError.message}</p>;
+
+const event = eventData?.event;
+const songRequests = songsData?.songRequests;
 
   const handleSongAdd = () => {
     navigate(`/events/${id}/add_song`);
-  }
+  };
 
-  const event = data?.event;
+  console.log("Event: ", event);
+  console.log("Song Requests: ", songRequests);
 
   return (
     <div>
-      <h1 className="text-4xl font-bold text-custom-charcoal">Event Details</h1>
-      <button 
-            className="btn btn-dark m-2"
-            onClick={handleSongAdd}
-          >
-            Add a Song
-          </button>
+      <h2 className="text-4xl font-bold text-custom-charcoal">{event.name}</h2>
+      <p className="text-2xl font-bold">{event.description}</p>
+      <h4>DJ: {event.user.username}</h4>
+      <button className="btn btn-dark m-2" onClick={handleSongAdd}>
+        Request a Song
+      </button>
       <SongsList eventId={id} />
     </div>
   );
